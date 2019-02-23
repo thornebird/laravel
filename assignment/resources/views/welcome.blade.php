8844d306b28e92e6245403780eca698973bc295b
thornebird <birdthorne@gmail.com>
2019-02-23 12:03:02 +0000

    conflict commit 3

--- assignment/resources/views/welcome.blade.php
+++ assignment/resources/views/welcome.blade.php
@@ -83,7 +83,9 @@
                 </div>
 
                 <div class="links">
-                    
+                    <a href="https://laravel.com/docs">Documentation</a>
+                    <a href="https://laracasts.com">Laracast feturess welcoe to lareavel 5</a>
+>>>>>>> feature
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
